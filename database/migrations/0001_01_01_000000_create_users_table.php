610c6ab9839a98cb4d111b8bc3314680f6812375
Thiago Catanho <thiagomcatanho@gmail.com>
2025-08-17 11:48:24 -0300

    feat: add migrations

---
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\Enums\UserRoles;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('document')->unique();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('role', UserRoles::cases())->index();
+            $table->string('position');
+            $table->date('birth_date');
+            $table->string('zip_code', 8);
+            $table->string('street');
+            $table->string('number');
+            $table->string('neighborhood');
+            $table->string('city');
+            $table->string('state', 2);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('sessions');
+    }
+};
