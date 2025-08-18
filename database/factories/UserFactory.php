29800b65843ee101d1d672e2cbd015ef2351cfe6
Thiago Catanho <thiagomcatanho@gmail.com>
2025-08-17 23:21:59 -0300

    refact: UserFactory and UserSeeder refactored

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,7 +5,6 @@ namespace Database\Factories;
 use App\Enums\UserRoles;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -25,29 +24,54 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name'        => $this->faker->name(),
-            'document'    => $this->faker->numerify('###########'),
-            'email'       => $this->faker->unique()->safeEmail(),
-            'password'    => Hash::make('password'),
-            'role'        => $this->faker->randomElement([UserRoles::ADMIN, UserRoles::EMPLOYEE]),
-            'position'    => $this->faker->jobTitle(),
-            'birth_date'  => $this->faker->date('Y-m-d', '-20 years'),
-            'zip_code'    => $this->faker->numerify('########'),
-            'street'      => $this->faker->streetName(),
-            'number'      => $this->faker->buildingNumber(),
-            'neighborhood' => $this->faker->citySuffix(),
-            'city'        => $this->faker->city(),
-            'state'       => $this->faker->stateAbbr(),
+            'name'        => fake()->name(),
+            'document'    => $this->generateValidCpf(),
+            'email'       => fake()->unique()->safeEmail(),
+            'password'    => 'secret_password',
+            'role' => fake()->randomElement(array_map(fn($r) => $r->value, UserRoles::cases())),
+            'position'    => fake()->jobTitle(),
+            'birth_date'  => fake()->date('Y-m-d', '-20 years'),
+            'zip_code'    => fake()->numerify('########'),
+            'street'      => fake()->streetName(),
+            'number'      => fake()->buildingNumber(),
+            'neighborhood' => fake()->citySuffix(),
+            'city'        => fake()->city(),
+            'state'       => fake()->stateAbbr(),
         ];
     }
 
     public function admin()
     {
-        return $this->state(fn() => ['role' => UserRoles::ADMIN]);
+        return $this->state(fn() => ['role' => UserRoles::ADMIN->value]);
     }
 
     public function employee()
     {
-        return $this->state(fn() => ['role' => UserRoles::EMPLOYEE]);
+        return $this->state(fn() => ['role' => UserRoles::EMPLOYEE->value]);
+    }
+
+    private function generateValidCpf(): string
+    {
+        $numbers = [];
+
+        for ($i = 0; $i < 9; $i++) {
+            $numbers[] = rand(0, 9);
+        }
+
+        $sum = 0;
+        for ($i = 0, $j = 10; $i < 9; $i++, $j--) {
+            $sum += $numbers[$i] * $j;
+        }
+        $remainder = $sum % 11;
+        $numbers[9] = ($remainder < 2) ? 0 : 11 - $remainder;
+
+        $sum = 0;
+        for ($i = 0, $j = 11; $i < 10; $i++, $j--) {
+            $sum += $numbers[$i] * $j;
+        }
+        $remainder = $sum % 11;
+        $numbers[10] = ($remainder < 2) ? 0 : 11 - $remainder;
+
+        return implode('', $numbers);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\UserAdmin;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -13,12 +14,23 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->admin()->create([
+        $admin = User::factory()->admin()->create([
             'name'     => 'Admin Master',
             'email'    => 'admin@example.com',
             'password' => Hash::make('admin123'),
         ]);
 
-        User::factory()->employee()->count(10)->create();
+        User::factory()
+            ->employee()
+            ->count(10)
+            ->create([
+                'password' => Hash::make('password')
+            ])
+            ->each(function ($employee) use ($admin) {
+                UserAdmin::create([
+                    'admin_id'    => $admin->id,
+                    'employee_id' => $employee->id,
+                ]);
+            });
     }
 }
